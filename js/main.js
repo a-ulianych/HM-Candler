3a53c97d5c05a0bdd20f339fc6da1f59f7e38279
a-ulianych <ulyanichaa@gmail.com>
2023-11-07 16:11:16 +0200

    Add feedback and certificates section

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ const menuMobile = document.querySelector('.header__menu-list');
 menuBtn.addEventListener('click', () => {
     menuMobile.classList.toggle('menu--open');
 });
+
+// Slider
+const swiperOne = new Swiper('.feedback__slider', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    spaceBetween: 30,
+});
+
+const swiperTwo = new Swiper('.certificates__slider', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    spaceBetween: 20,
+    slidesPerView: 3,
+});
