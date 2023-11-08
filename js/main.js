2d7654c58d8ba23636038513ddf4d38b125879c2
a-ulianych <ulyanichaa@gmail.com>
2023-11-08 23:29:48 +0200

    Add questions section

--- js/main.js
+++ js/main.js
@@ -34,3 +34,22 @@ const swiperTwo = new Swiper('.certificates__slider', {
         }
     },
 });
+
+//Accordeon
+const accordeon = document.querySelector('.accordeon');
+const accordeonTitles = accordeon.querySelectorAll('.accordeon__title');
+
+accordeonTitles.forEach.call(accordeonTitles, function (accordionTitle) {
+    accordionTitle.addEventListener('click', () => {
+        const currentText = accordionTitle.parentElement.querySelector('.accordeon__text');
+
+        accordionTitle.classList.toggle('accordeon__title--active');
+        currentText.classList.toggle('accordeon__text--visible');
+
+        if (currentText.classList.contains('accordeon__text--visible')) {
+            currentText.style.maxHeight = currentText.scrollHeight + 'px';
+        } else {
+            currentText.style.maxHeight = null;
+        }
+    });
+});
